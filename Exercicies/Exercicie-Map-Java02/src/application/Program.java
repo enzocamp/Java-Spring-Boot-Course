3ccba1cf430d4947e8dd9fcbe34a26bbc827298a
Enzo Camp <enzo.flap@hotmail.com>
2025-04-05 18:23:31 -0300

    Treinando Collection HashMap

--- Exercicies/Exercicie-Map-Java02/src/application/Program.java
+++ Exercicies/Exercicie-Map-Java02/src/application/Program.java
@@ -8,24 +8,44 @@ public class Program {
 	public static void main(String[] args) {
 		//Write a Java program to copy all mappings from the specified map to another map.
 		
+		//Contravariância
+		
 		Map<String, Double> mapOne = new HashMap<>();
 		
 		mapOne.put("Teste1", 10.00);
 		mapOne.put("Teste2", 54.00);
 		mapOne.put("Teste3", 3.50);
 		
-		Map<? super Object, ? super Object> mapTwo = new HashMap<>();
+		//Map<? super Object, ? super Object> mapTwo = new HashMap<>();
 		
-		mapTwo.put(1, "now");
+		//mapTwo.put(1, "now");
 		
-		mapTwo.putAll(mapOne);
+		//mapTwo.putAll(mapOne);
 		
 		/*mapOne.forEach((key, value) -> {
 			mapTwo.put(key, value);
 		});*/
 		
-		System.out.println(mapOne);
-		System.out.println(mapTwo);
-	}
+		//System.out.println(mapOne);
+		//System.out.println(mapTwo);
+		
+		//Covariância
+		
+		  Map<String, Double> mapTwo = new HashMap<>();
+	        
+	        // Passa mapOne para o método que aceita qualquer subtipo de Map
+	        copyMap(mapOne, mapTwo);
+
+	        System.out.println(mapOne);
+	        System.out.println(mapTwo);
+	    }
+
+	    // Método genérico com ? extends para leitura segura
+	    public static <K, V> void copyMap(Map<? extends K, ? extends V> source, Map<K, V> target) {
+	        source.forEach((key, value) -> target.put(key, value));
+	    }
+	///}
+		
+	//}
 
 }
