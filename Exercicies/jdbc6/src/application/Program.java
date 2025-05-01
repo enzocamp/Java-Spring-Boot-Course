011f1b2e185d6a5e43722f5cbfefc86e6c45b566
Enzo Camp <enzo.flap@hotmail.com>
2025-05-01 16:21:41 -0300

    Utilizando JDBC e Transactions

---
+++ Exercicies/jdbc6/src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+
+		Statement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			conn.setAutoCommit(false); // só acontecerá as operações quando confirma-las será efetuadas;
+
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+			// int x = 1;
+			// if (x < 2) {
+			// throw new SQLException("Fake error");
+			// }
+
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+			conn.commit(); // Aqui estaria a confirmação
+
+			System.out.println("rows1 = " + rows1);
+			System.out.println("rows2 = " + rows2);
+			
+
+		} catch (SQLException e) {
+			try {
+				conn.rollback(); // caso aconteça erro, o banco volta o estado antes das alterações
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		}
+
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+
+		}
+	}
+
+}
